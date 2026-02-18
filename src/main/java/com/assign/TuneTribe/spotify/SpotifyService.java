8992c435636a07ee3143b206e21619f8311ac3bb
Shauna <shaunaayscue7@gmail.com>
2026-02-18 17:14:24 -0500

    fix

--- src/main/java/com/assign/TuneTribe/spotify/SpotifyService.java
+++ src/main/java/com/assign/TuneTribe/spotify/SpotifyService.java
@@ -1,21 +1,15 @@
 package com.assign.TuneTribe.spotify;
 
 import java.time.Instant;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.*;
+
+import org.slf4j.*;
+
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.util.*;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
